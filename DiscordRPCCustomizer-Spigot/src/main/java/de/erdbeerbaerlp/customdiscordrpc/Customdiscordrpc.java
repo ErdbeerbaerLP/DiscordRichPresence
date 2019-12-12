620501daf99e8cfc88567d2f309515a924dfd7c6
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-12-12 23:05:29 +0100

    [Spigot] Add new config to disable logging of requests

--- DiscordRPCCustomizer-Spigot/src/main/java/de/erdbeerbaerlp/customdiscordrpc/Customdiscordrpc.java
+++ DiscordRPCCustomizer-Spigot/src/main/java/de/erdbeerbaerlp/customdiscordrpc/Customdiscordrpc.java
@@ -18,6 +18,7 @@ public class Customdiscordrpc extends JavaPlugin implements PluginMessageListene
     Properties config = new Properties();
     String discordMsg = "Missingno.";
     String discordIconKey = "world";
+    boolean logging = false;
     
     public void onEnable() {
         try {
@@ -26,6 +27,7 @@ public class Customdiscordrpc extends JavaPlugin implements PluginMessageListene
                 System.out.println("Generating new config file!");
                 this.config.setProperty("message", "Playing on a random Server with %otherpl% other players");
                 this.config.setProperty("iconKey", "world");
+                this.config.setProperty("logRequests", "false");
                 this.config.store(new FileWriter(this.msgFile),
                                   "DiscordRPC Customizer config file\n\nPlaceholders for message:\n%players% for total players on this server\n%otherpl% for total players -1\n\niconKey: The iconKey used to show an icon.\nYou can use \"cube\" or \"world\" by default.\nContact me if you have an big server and want an iconKey for you server\n");
             }
@@ -33,6 +35,7 @@ public class Customdiscordrpc extends JavaPlugin implements PluginMessageListene
             this.config.load(new FileInputStream(this.msgFile));
             this.discordMsg = this.config.getProperty("message", "Playing on a random Server with %otherpl% other players");
             this.discordIconKey = this.config.getProperty("iconKey", "world");
+            this.logging = this.config.getProperty("logRequests", "false").equalsIgnoreCase("true");
         } catch (IOException var2) {
             System.err.println("ERROR READING/CREATING MESSAGE FILE...\n" + var2);
         }
@@ -53,7 +56,7 @@ public class Customdiscordrpc extends JavaPlugin implements PluginMessageListene
     public void onPluginMessageReceived(String arg0, Player arg1, byte[] arg2) {
         String str = (new String(arg2, StandardCharsets.UTF_8)).trim();
         if (arg0.equals("discordrpc:discord-req") && str.equals("DRPC-Message-Request")) {
-            System.out.println("Client requested Discord RichPresence Message... Trying to send it");
+            if(logging) System.out.println("Client requested Discord RichPresence Message... Trying to send it");
             this.sendMessage(arg1, this.discordMsg, "discordrpc:discord-msg");
             this.sendMessage(arg1, this.discordIconKey, "discordrpc:discord-icon");
         }
